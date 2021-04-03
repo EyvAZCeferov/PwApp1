a9ae158427fd126858e5c5f46eb1fa41832d5943
EyvAZCeferov <eyvaz.ceferov@gmail.com>
2021-04-03 23:18:09 +0400

    Paythanks success

---
+++ src/screens/tabs/barcode/paythanks.js
@@ -0,0 +1,173 @@
+import * as React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+} from "react-native";
+import { Button } from "native-base";
+
+import { AntDesign } from "@expo/vector-icons";
+import { t } from "../../../functions/lang";
+import { QRCode as CustomQRCode } from "react-native-custom-qr-codes-expo";
+import { StatusBar } from "expo-status-bar";
+import Textpopins from "../../../functions/screenfunctions/text";
+const { width, height } = Dimensions.get("window");
+
+function Simple(props) {
+  return (
+    <CustomQRCode
+      style={styles.barcode}
+      content="Content is here"
+      size={width / 1.25}
+      color="#fff"
+      codeStyle="square"
+      innerEyeStyle="square"
+      outerEyeStyle="square"
+    />
+  );
+}
+
+export default class PayThanks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refresh: true,
+      link: null,
+    };
+  }
+
+  componentDidMount() {
+    const params = this.props.route.params;
+    if (params.checkid != null) {
+      this.setState({ link: params.checkid, refresh: false });
+      this.renderContent();
+    }
+  }
+
+  renderContent() {
+    if (this.state.refresh) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <ActivityIndicator
+            size="large"
+            color="#7c9d32"
+            animating={true}
+            focusable={true}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={[styles.f1, styles.bgGreen]}>
+          <View style={styles.thanksArena}>
+            <View style={styles.barcode}>
+              <Simple {...this.props} link={this.state.link} />
+            </View>
+            <Textpopins
+              style={[styles.icon, styles.title]}
+              children={t("barcode.thanksBuying.title")}
+            />
+            <Textpopins
+              style={styles.subtitle}
+              children={t("barcode.thanksBuying.scanCode")}
+            />
+            <View style={styles.centerItems}>
+              <Button
+                rounded
+                iconLeft
+                light
+                style={styles.btn}
+                onPress={() => this.props.navigation.navigate("Home")}
+              >
+                <AntDesign name="home" size={24} color="black" />
+                <Textpopins
+                  style={styles.btnText}
+                  children={t("barcode.thanksBuying.returnHome")}
+                />
+              </Button>
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <StatusBar backgroundColor="#7c9d32" style="light" />
+        {this.renderContent()}
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bgGreen: {
+    backgroundColor: "#7c9d32",
+  },
+  f1: {
+    width,
+    height,
+  },
+  thanksArena: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  icon: {
+    fontSize: 50,
+    color: "#fff",
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  barcode: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    marginBottom: 35,
+    color: "#fff",
+  },
+  w1h1: {
+    width: 500,
+    height: 500,
+  },
+  title: {
+    fontSize: 30,
+    marginVertical: 10,
+    marginTop: 20,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "#fff",
+    margin: 0,
+    textAlign: "center",
+  },
+  centerItems: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  btn: {
+    padding: 15,
+    marginTop: 20,
+  },
+  btnText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingLeft: 5,
+    color: "#010101",
+  },
+});
--- src/screens/tabs/bucket/cartlists.js
+++ src/screens/tabs/bucket/cartlists.js
@@ -1,146 +1,189 @@
 import React from "react";
-import {View, Text, StyleSheet, Dimensions, FlatList, SafeAreaView} from "react-native";
-import {StatusBar} from "expo-status-bar";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
 import BucketHeader from "./components/BucketHeader";
-import {t} from "../../../functions/lang";
-import {List, ListItem, Left, Body, Right, Button, Thumbnail} from "native-base";
-import {connect} from "react-redux";
+import { t } from "../../../functions/lang";
+import {
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Button,
+  Thumbnail,
+  Fab,
+} from "native-base";
+import { connect } from "react-redux";
 import Constants from "expo-constants";
 import Textpopins from "../../../functions/screenfunctions/text";
-import {EvilIcons} from "@expo/vector-icons";
-import NumericInput from 'react-native-numeric-input'
+import { EvilIcons, AntDesign } from "@expo/vector-icons";
+import NumericInput from "react-native-numeric-input";
 
-
-const {width, height} = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 function CartList(props) {
-
-    function renderBucket({item, index}) {
-
-        return (
-            <ListItem key={index} style={{marginLeft: -5}} onPress={() =>
-                props.navigation.navigate("ProductInfo", {
-                    uid: item.id,
-                })
-            }>
-                <Left style={{maxWidth: width / 6}}>
-                    <Thumbnail source={{uri: item.image}} style={{maxWidth: "100%"}}/>
-                </Left>
-                <Body style={{maxWidth: width / 3 + 30}}>
-                    <Textpopins children={item.name}/>
-                </Body>
-                <Right style={{flexDirection: "row"}}>
-                    <NumericInput
-                        value={Math.ceil(item.qyt)}
-                        onChange={value => props.updateVal(item, value)}
-                        onLimitReached={(isMax, msg) => alert(isMax, msg)}
-                        totalWidth={width / 3}
-                        totalHeight={50}
-                        iconSize={25}
-                        step={1}
-                        minValue={1}
-                        maxValue={100}
-                        valueType='integer'
-                        rounded
-                        textColor='#7c9d32'
-                        iconStyle={{color: 'white'}}
-                        rightButtonBackgroundColor='#7c9d32'
-                        leftButtonBackgroundColor='#E56B70'/>
-
-                    <Button transparent style={styles.buttonStyle} onPress={() => props.removeCart(item)}>
-                        <EvilIcons name="trash" size={30} color="#BF360C"/>
-                    </Button>
-
-                </Right>
-            </ListItem>
-        );
-    }
-
+  function renderBucket({ item, index }) {
     return (
-        <SafeAreaView style={[styles.container, styles.center]}>
-            <StatusBar backgroundColor="#fff" style="dark"/>
-            <View>
-                <View style={styles.header}>
-                    <BucketHeader
-                        button={true}
-                        title={t("bucket.header.cartlists.title")}
-                    />
-                </View>
-                <View style={[styles.center, styles.contentArena]}>
-                    {props.bucketitems && props.bucketitems.length > 0 ? (
-                        <List
-                            style={{
-                                width: width,
-                                height: "100%",
-                                margin: 0,
-                                padding: 0,
-                            }}
-                        >
-                            <FlatList
-                                data={props.bucketitems}
-                                renderItem={renderBucket}
-                                keyExtractor={(item, index) => index.toString()}
-                            />
-                        </List>
-                    ) : (
-                        <Textpopins
-                            children={t("actions.noResult")}
-                            style={styles.noResult}
-                        />
-                    )}
-                </View>
-            </View>
-        </SafeAreaView>
+      <ListItem
+        key={index}
+        style={{ marginLeft: -5 }}
+        onPress={() =>
+          props.navigation.navigate("ProductInfo", {
+            uid: item.id,
+          })
+        }
+      >
+        <Left style={{ maxWidth: width / 6 }}>
+          <Thumbnail
+            source={{ uri: item.image }}
+            style={{ maxWidth: "100%" }}
+          />
+        </Left>
+        <Body style={{ maxWidth: width / 3 + 30 }}>
+          <Textpopins children={item.name} />
+        </Body>
+        <Right style={{ flexDirection: "row" }}>
+          <NumericInput
+            value={Math.ceil(item.qyt)}
+            onChange={(value) => props.updateVal(item, value)}
+            onLimitReached={(isMax, msg) => alert(isMax, msg)}
+            totalWidth={width / 3}
+            totalHeight={50}
+            iconSize={25}
+            step={1}
+            minValue={1}
+            maxValue={100}
+            valueType="integer"
+            rounded
+            textColor="#7c9d32"
+            iconStyle={{ color: "white" }}
+            rightButtonBackgroundColor="#7c9d32"
+            leftButtonBackgroundColor="#E56B70"
+          />
+
+          <Button
+            transparent
+            style={styles.buttonStyle}
+            onPress={() => props.removeCart(item)}
+          >
+            <EvilIcons name="trash" size={30} color="#BF360C" />
+          </Button>
+        </Right>
+      </ListItem>
     );
+  }
+
+  return (
+    <SafeAreaView style={[styles.container, styles.center]}>
+      <StatusBar backgroundColor="#fff" style="dark" />
+      <View>
+        <View style={styles.header}>
+          <BucketHeader
+            button={true}
+            title={t("bucket.header.cartlists.title")}
+          />
+        </View>
+        <View style={[styles.center, styles.contentArena]}>
+          {props.bucketitems && props.bucketitems.length > 0 ? (
+            <List
+              style={{
+                width: width,
+                height: "100%",
+                margin: 0,
+                padding: 0,
+              }}
+            >
+              <Fab
+                direction="right"
+                position="bottomRight"
+                style={{
+                  backgroundColor: "#7c9d32",
+                  zIndex: 150,
+                }}
+                onPress={() =>
+                  props.navigation.navigate("PayThanks", {
+                    checkid: 2,
+                  })
+                }
+              >
+                <AntDesign name="check" size={24} color="#fff" />
+              </Fab>
+
+              <FlatList
+                data={props.bucketitems}
+                renderItem={renderBucket}
+                keyExtractor={(item, index) => index.toString()}
+              />
+            </List>
+          ) : (
+            <Textpopins
+              children={t("actions.noResult")}
+              style={styles.noResult}
+            />
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        removeCart: (product) => dispatch({type: "REMOVE_FROM_CART", payload: product}),
-        updateVal: (product, value) => dispatch({type: "UPDATE_CART", payload: product, value: value}),
-    };
+  return {
+    removeCart: (product) =>
+      dispatch({ type: "REMOVE_FROM_CART", payload: product }),
+    updateVal: (product, value) =>
+      dispatch({ type: "UPDATE_CART", payload: product, value: value }),
+  };
 };
 
 const mapStateToProps = (state) => {
-    return {
-        bucketitems: state.bucketitems,
-    };
+  return {
+    bucketitems: state.bucketitems,
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartList);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
-    header: {
-        width: width,
-        height: "20%",
-        marginTop: Constants.statusBarHeight,
-    },
-    contentArena: {
-        width: width,
-        height: "80%",
-        flexDirection: "column",
-        margin: 0,
-        padding: 0
-    },
-    center: {
-        textAlign: "center",
-        justifyContent: "center",
-        alignContent: "center",
-        alignItems: "center",
-    },
-    noResult: {
-        color: "#D50000",
-        textAlign: "center",
-        fontSize: 20,
-        fontWeight: "700",
-    },
-    buttonstyle: {
-        alignContent: "center",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    width: width,
+    height: "20%",
+    marginTop: Constants.statusBarHeight,
+  },
+  contentArena: {
+    width: width,
+    height: "80%",
+    flexDirection: "column",
+    margin: 0,
+    padding: 0,
+    marginTop: -Constants.statusBarHeight,
+  },
+  center: {
+    textAlign: "center",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  noResult: {
+    color: "#D50000",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  buttonstyle: {
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
